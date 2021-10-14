83091ff0150dab3d99c309e1d60f5bb5b1b96c82
Euan <euan.workie@gmail.com>
2021-10-14 10:43:34 +0200

    - Merged API requests into 1  - Converted API calls to receive body parameters instead of path parameters

--- src/main/java/com/example/springplayground/api/issue/IssueController.java
+++ src/main/java/com/example/springplayground/api/issue/IssueController.java
@@ -2,12 +2,13 @@ package com.example.springplayground.api.issue;
 
 import com.example.springplayground.controller.api.IssuesApi;
 import com.example.springplayground.controller.model.IssueDTO;
-import com.example.springplayground.service.model.Issue;
+import com.example.springplayground.controller.model.SearchIssues;
 import com.example.springplayground.util.MapperUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,49 +20,57 @@ public class IssueController implements IssuesApi {
 
     private final IssueService issueService;
 
-    private final List<IssueDTO> issuesGroup = new ArrayList<>();
+    private List<IssueDTO> comicIssues = new ArrayList<>();
+    private List<IssueDTO> mappedIssues;
 
     public IssueController(IssueService issueService) {
         this.issueService = issueService;
     }
 
+    @Override
+    public ResponseEntity<List<IssueDTO>> getIssues(SearchIssues searchIssues) {
+
+        return ResponseEntity.ok(new ArrayList<>());
+
+        // List<com.example.springplayground.service.model.Issue> issues = issueService.getIssues();
+        // mappedIssues = MapperUtil.mapList(issues, IssueDTO.class);
+        //
+        // if (!comicIssues.isEmpty()) comicIssues.clear();
+        //
+        // if (searchIssues.getTitle() == null) {
+        //     int iterations = 5;
+        //
+        //     IntStream.range(0, iterations)
+        //             .forEach(index -> comicIssues.addAll(mappedIssues));
+        // } else {
+        //     String issueName = searchIssues.getTitle().toLowerCase();
+        //
+        //     comicIssues = mappedIssues
+        //             .stream()
+        //             .filter(issue -> {
+        //                 String issueTitle = issue.getTitle().toLowerCase();
+        //                 return issueTitle.contains(issueName);
+        //             })
+        //             .collect(Collectors.toList());
+        // }
+        //
+        // return ResponseEntity.ok(comicIssues);
+    }
+
+
     // @Override
-    // public ResponseEntity<IssueDTO> getIssueById(BigDecimal id) {
-    //     Issue issue = issueService.getIssue(id);
-    //     return ResponseEntity.ok(MapperUtil.mapObject(issue, IssueDTO.class));
+    // public ResponseEntity<List<IssueDTO>> getIssueList() {
+    //
+    //     int iterations = 5;
+    //
+    //     if (!comicIssues.isEmpty()) comicIssues.clear();
+    //
+    //     List<Issue> issues = issueService.getIssues();
+    //     List<IssueDTO> mappedIssues = MapperUtil.mapList(issues, IssueDTO.class);
+    //
+    //     IntStream.range(0, iterations)
+    //             .forEach(index -> comicIssues.addAll(mappedIssues));
+    //
+    //     return ResponseEntity.ok(comicIssues);
     // }
-
-    @Override
-    public ResponseEntity<List<IssueDTO>> getIssueByName(String searchTerm) {
-
-        String issueName = searchTerm.toLowerCase();
-
-        List<Issue> issues = issueService.getIssues();
-        List<IssueDTO> mappedIssues = MapperUtil.mapList(issues, IssueDTO.class);
-
-        List<IssueDTO> result = mappedIssues.stream().filter(issue -> {
-                    String title = issue.getTitle().toLowerCase();
-
-                    return title.contains(issueName);
-                })
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(result);
-    }
-
-    @Override
-    public ResponseEntity<List<IssueDTO>> getIssueList() {
-
-        int iterations = 5;
-
-        if (!issuesGroup.isEmpty()) issuesGroup.clear();
-
-        List<Issue> issues = issueService.getIssues();
-        List<IssueDTO> mappedIssues = MapperUtil.mapList(issues, IssueDTO.class);
-
-        IntStream.range(0, iterations)
-                .forEach(index -> issuesGroup.addAll(mappedIssues));
-
-        return ResponseEntity.ok(issuesGroup);
-    }
 }
