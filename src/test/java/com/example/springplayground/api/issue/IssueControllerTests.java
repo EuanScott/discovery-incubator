e6e797981679dd81605680e63a10950f948aaea9
Euan <euan.workie@gmail.com>
2021-10-28 19:06:08 +0200

    - Updates to testing implementation

--- src/test/java/com/example/springplayground/api/issue/IssueControllerTests.java
+++ src/test/java/com/example/springplayground/api/issue/IssueControllerTests.java
@@ -1,40 +1,53 @@
 package com.example.springplayground.api.issue;
 
-import com.example.springplayground.SpringPlaygroundApplication;
-import com.example.springplayground.controller.model.IssueDTO;
-import com.example.springplayground.controller.model.SearchIssues;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 
-import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
-@WebMvcTest(IssueController.class)
-@ContextConfiguration(classes = SpringPlaygroundApplication.class)
-@ActiveProfiles({"test"})
+// @SpringBootTest
+// @WebMvcTest(IssueController.class)
+// @ContextConfiguration(classes = SpringPlaygroundApplication.class)
+// @ActiveProfiles({"test"})
 class IssueControllerTests {
 
-    @Autowired
-    private IssueService issueService;
+    // @Autowired
+    // private IssueService issueService;
+
+    // @Test
+    // @DisplayName("List Issue 200 OK")
+    // void testIssueList() {
+    //     // given
+    //     IssueController issueController = new IssueController(issueService);
+    //     SearchIssues searchIssues = new SearchIssues();
+    //
+    //     // when
+    //     ResponseEntity<List<IssueDTO>> testIssues = issueController.getIssues(searchIssues);
+    //
+    //     // then
+    //     Assertions.assertEquals("200", testIssues.getStatusCode().toString());
+    // }
+
+    Calculator underTest = new Calculator();
 
     @Test
-    @DisplayName("List Issue 200 OK")
-    void testIssueList() {
+    void itShouldAddTwoNumbers() {
         // given
-        IssueController issueController = new IssueController(issueService);
-        SearchIssues searchIssues = new SearchIssues();
+        int numberOne = 20;
+        int numberTwo = 30;
 
         // when
-        ResponseEntity<List<IssueDTO>> testIssues = issueController.getIssues(searchIssues);
+        int result = underTest.add(numberOne, numberTwo);
 
         // then
-        Assertions.assertEquals("200", testIssues.getStatusCode().toString());
+        int expected = 50;
+        assertThat(result).isEqualTo(expected);
     }
+
+    static class Calculator {
+        int add(int a, int b) {
+            return a + b;
+        }
+    }
+
+
 }
