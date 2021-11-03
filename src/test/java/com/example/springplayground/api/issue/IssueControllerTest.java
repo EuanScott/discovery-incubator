a5c5d6664769861f12626691f03b06dae5e99315
Euan <euan.workie@gmail.com>
2021-11-03 07:00:33 +0200

    Updates to Unit Testing

--- src/test/java/com/example/springplayground/api/issue/IssueControllerTest.java
+++ src/test/java/com/example/springplayground/api/issue/IssueControllerTest.java
@@ -1,23 +1,35 @@
 package com.example.springplayground.api.issue;
 
 import com.example.springplayground.controller.model.IssueDTO;
+import com.example.springplayground.controller.model.SearchIssues;
 import com.example.springplayground.setup.WireMockService;
 import org.apache.http.impl.conn.Wire;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.annotation.PreDestroy;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ActiveProfiles("test")
+@TestConfiguration
 class IssueControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    // @Autowired
     private IssueController issueControllerTest;
+
+    @Autowired
+    private IssueService issueService;
 
     // @Autowired
     // private WireMockService wireMockService;
@@ -26,6 +38,11 @@ class IssueControllerTest {
     // public void setup() {
     //     this.mockMvc = MockMvcBuilders.standaloneSetup(issueControllerTest).build();
     // }
+
+    IssueControllerTest(IssueController issueController) {
+        WireMockService.beforeAll();
+        this.issueControllerTest = issueController;
+    }
 
     @Test
     public void contextLoads() throws Exception {
@@ -36,11 +53,16 @@ class IssueControllerTest {
          * 3) Validate successful response
          */
 
-        WireMockService.beforeAll();
 
         IssueDTO issue = new IssueDTO();
 
         System.out.println(issue);
+
+        // "this.issueControllerTest" is null
+        SearchIssues searchIssues = new SearchIssues();
+        ResponseEntity<List<IssueDTO>> result = issueControllerTest.getIssues(searchIssues);
+
+        assertThat(result.getStatusCode().toString().equals("200"));
 
         // mockMvc.perform(
         //                 post("api/issues")
@@ -59,17 +81,16 @@ class IssueControllerTest {
         // assertThat(testClient.get("/some/thing/else").statusCode(), is(404));
 
 
-
         // given
-        int numberOne = 20;
-        int numberTwo = 30;
-
-        // when
-        int result = numberOne + numberTwo;
-
-        // then
-        int expected = 50;
-        assertThat(result).isEqualTo(expected);
+        // int numberOne = 20;
+        // int numberTwo = 30;
+        //
+        // // when
+        // int result = numberOne + numberTwo;
+        //
+        // // then
+        // int expected = 50;
+        // assertThat(result).isEqualTo(expected);
 
         WireMockService.afterAll();
     }
