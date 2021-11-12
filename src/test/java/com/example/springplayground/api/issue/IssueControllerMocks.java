8cbc5e0fe4b2f86edb3b022f3094b3e24f40e0f5
EuanScott-Disco <92733207+EuanScott-Disco@users.noreply.github.com>
2021-11-12 07:33:24 +0200

    Worked on Mockito tests

--- src/test/java/com/example/springplayground/api/issue/IssueControllerMocks.java
+++ src/test/java/com/example/springplayground/api/issue/IssueControllerMocks.java
@@ -1,35 +1,46 @@
 package com.example.springplayground.api.issue;
 
+import com.example.springplayground.SpringPlaygroundApplication;
 import com.example.springplayground.controller.model.IssueDTO;
 import com.example.springplayground.controller.model.SearchIssues;
-import com.example.springplayground.service.model.*;
-import com.google.gson.Gson;
-import io.micrometer.core.instrument.search.Search;
-import org.junit.Rule;
+import com.example.springplayground.setup.TestApplicationConfiguration;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.MockitoRule;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.math.BigDecimal;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
+@ActiveProfiles("test")
+@ContextConfiguration(classes = {SpringPlaygroundApplication.class, TestApplicationConfiguration.class})
+@SpringBootTest
+@WebAppConfiguration
 public class IssueControllerMocks {
 
     // @Rule
     // public MockitoRule initRule = MockitoJUnit.rule();
 
-    @Mock
-    IssueService issueService;
+    // @Mock
+    // IssueService issueService;
+
     @Mock
     IssueController issueController;
 
@@ -43,18 +54,28 @@ public class IssueControllerMocks {
      *      -> I feel I should be calling the controller directly as that is what the user would interact with and not care about the service
      */
     @Test
-    public void mockGetIssue() {
-        System.out.println("Doing Mockito");
-
-        List<IssueDTO> customResponse = Arrays.asList(new IssueDTO(), new IssueDTO());
-        System.out.println("customResponse: " + customResponse);
-
+    public void mockGetIssues() {
         SearchIssues searchIssues = new SearchIssues();
-        ResponseEntity<List<IssueDTO>> apiResponse = issueController.getIssues(searchIssues);
-        System.out.println("apiResponse: " + apiResponse);
+        ResponseEntity<List<IssueDTO>> customResponse = ResponseEntity.ok(Arrays.asList(new IssueDTO(), new IssueDTO()));
 
-        when(issueController.getIssues(searchIssues)).thenReturn((ResponseEntity<List<IssueDTO>>) customResponse);
+        when(issueController.getIssues(searchIssues)).thenReturn(customResponse);
 
-        assertEquals(issueService.getIssues(), apiResponse);
+        assertEquals(customResponse, issueController.getIssues(searchIssues));
     }
+
+    // @Test
+    // public void mockGetIssue() throws FileNotFoundException, ParseException {
+    //     JSONParser parser = new JSONParser();
+    //     Object jsonObject = parser.parse(new FileReader("C:/Users/p01euans/Desktop/list_test.json"));
+    //     IssueDTO myObj = (IssueDTO)jsonObject;
+    //     System.out.println(myObj);
+    //
+    //     ResponseEntity<IssueDTO> customResponse = ResponseEntity.ok(myObj);
+    //
+    //     when(issueController.getIssueById(BigDecimal.valueOf(42))).thenReturn(customResponse);
+    //
+    //     assertThat(issueController.getIssueById(BigDecimal.valueOf(42))).hasFieldOrProperty("title");
+    //
+    //     assertEquals(customResponse, issueController.getIssueById(BigDecimal.valueOf(42)));
+    // }
 }
