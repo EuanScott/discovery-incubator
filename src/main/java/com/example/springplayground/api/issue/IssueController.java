e72edbf015abb84f73f985284272f0d931ac348e
EuanScott-Disco <92733207+EuanScott-Disco@users.noreply.github.com>
2021-11-18 19:17:50 +0200

    Updates to Wiremock testing (Thanks Josh)

--- src/main/java/com/example/springplayground/api/issue/IssueController.java
+++ src/main/java/com/example/springplayground/api/issue/IssueController.java
@@ -6,24 +6,19 @@ import com.example.springplayground.controller.model.SearchIssues;
 import com.example.springplayground.service.model.Issue;
 import com.example.springplayground.util.MapperUtil;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("/api")
 public class IssueController implements IssuesApi {
 
     private final IssueService issueService;
-
-    private List<IssueDTO> comicIssues = new ArrayList<>();
-    private List<IssueDTO> mappedIssues;
 
     public IssueController(IssueService issueService) {
         this.issueService = issueService;
@@ -37,27 +32,22 @@ public class IssueController implements IssuesApi {
 
     @Override
     public ResponseEntity<List<IssueDTO>> getIssues(SearchIssues searchIssues) {
-
         List<Issue> issues = issueService.getIssues();
-        mappedIssues = MapperUtil.mapList(issues, IssueDTO.class);
+        List<IssueDTO> mappedIssues = MapperUtil.mapList(issues, IssueDTO.class);
 
-        if (!comicIssues.isEmpty()) comicIssues.clear();
-
-        if (searchIssues.getTitle() == null || Objects.equals(searchIssues.getTitle(), "")) {
-            int iterations = 5;
-
-            IntStream.range(0, iterations)
-                    .forEach(index -> comicIssues.addAll(mappedIssues));
-        } else {
-            String issueName = searchIssues.getTitle().toLowerCase();
+        List<IssueDTO> comicIssues;
+        if (StringUtils.hasText(searchIssues.getTitle())) {
+            String issueNameInLowercase = searchIssues.getTitle().toLowerCase();
 
             comicIssues = mappedIssues
                     .parallelStream()
-                    .filter(issue -> {
-                        String issueTitle = issue.getTitle().toLowerCase();
-                        return issueTitle.contains(issueName);
-                    })
+                    .filter(issue -> issue.getTitle().toLowerCase().contains(issueNameInLowercase))
                     .collect(Collectors.toList());
+        } else {
+            // Used for when I want to return 30 comic issues
+            // IntStream.range(0, 5)
+            //         .forEach(index -> comicIssues.addAll(mappedIssues));
+            comicIssues = mappedIssues;
         }
 
         return ResponseEntity.ok(comicIssues);
--- src/test/java/com/example/springplayground/api/issue/IssueControllerTestWithWiremock.java
+++ src/test/java/com/example/springplayground/api/issue/IssueControllerTestWithWiremock.java
@@ -5,13 +5,10 @@ import com.example.springplayground.controller.model.IssueDTO;
 import com.example.springplayground.controller.model.SearchIssues;
 import com.example.springplayground.setup.TestApplicationConfiguration;
 import com.github.tomakehurst.wiremock.WireMockServer;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,10 +17,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("test")
 @ContextConfiguration(classes = {SpringPlaygroundApplication.class, TestApplicationConfiguration.class})
@@ -38,12 +37,6 @@ public class IssueControllerTestWithWiremock {
     @Autowired
     private IssueController issueController;
 
-    @Autowired
-    @Qualifier("retrofitServiceEnv")
-    private String wireMockURL;
-
-    private final CloseableHttpClient httpClient = HttpClients.createDefault();
-
     @Before
     public void setup() {
         wireMockServer.resetMappings();
@@ -52,114 +45,56 @@ public class IssueControllerTestWithWiremock {
     }
 
     @Test
-    public void getIssuesHttpOkay() throws IOException {
+    public void getIssuesWithoutSearchTerm() {
 
-        // Call a fake server to return me a hard-coded response
-        wireMockServer.stubFor(post(urlMatching("/api/issues"))
-                .withRequestBody(equalToJson("{\"title\": null}"))
+        // Create a fake server to return me a hard-coded response from the Down Stream
+        wireMockServer.stubFor(get(urlEqualTo("/Issues"))
                 .willReturn(aResponse()
                         .withStatus(HttpStatus.OK.value())
-                        .withBodyFile("src/test/resources/__files/issue_test.json")
+                        .withBodyFile("issue_test.json")
                 )
         );
 
-
-        /**
-         * TODO:
-         *  I am unable to call the service directly -> "use a service wrapper that uses a service bean of which that service bean is pointing to the wiremock serverl."
-         */
+        // Call the issue controller that will be using my fake server to return me the pre-defined response body
         SearchIssues searchIssues = new SearchIssues();
-        ResponseEntity<List<IssueDTO>> issues = issueController.getIssues(searchIssues);
+        ResponseEntity<List<IssueDTO>> issuesResponse = issueController.getIssues(searchIssues);
 
-        System.out.println(issues);
+        // Base check: Make sure that the response object exists
+        assertNotNull(issuesResponse);
 
+        // Make sure that the amount of Issues being returned is the correct amount
+        List<IssueDTO> issues = issuesResponse.getBody();
+        assertNotNull(issues);
+        assertEquals(2, issues.size());
 
-        // HttpPost request = new HttpPost(wireMockURL + "/api/issues");
-        //
-        // request.setHeader("Accept", "application/json");
-        // request.setHeader("Content-type", "application/json");
-        //
-        // String json = "{ \"title\": null}";
-        // StringEntity params = new StringEntity(json);
-        // request.setEntity(params);
-        //
-        // HttpResponse httpResponse = httpClient.execute(request);
-        //
-        // int responseStatusCode = httpResponse.getStatusLine().getStatusCode();
-        // System.out.println("Response Status Code: " + responseStatusCode);
-
-        // String responseString = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
-        // System.out.println(responseString);
-        //
-        // assertEquals(200, responseStatusCode);
+        // Check that the Issue response being returned has the correct response that I have mocked out earlier
+        IssueDTO issue = issues.get(0);
+        assertNotNull(issue);
+        assertEquals(BigDecimal.valueOf(58758), issue.getId());
+        assertEquals("Moon Girl and Devil Dinosaur (2015) #5 (Guerra Wop Variant)", issue.getTitle());
     }
 
-    // @Test
-    // public void getIssuesHttpNotOkay() throws IOException {
-    //     System.out.println("getIssues A-Not-Okay");
-    //
-    //     wireMockServer.stubFor(post(urlMatching("/api/issues"))
-    //             .withRequestBody(equalToJson("{\"title\": null}"))
-    //             .willReturn(aResponse()
-    //                     .withStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
-    //             )
-    //     );
-    //
-    //     HttpPost request = new HttpPost(wireMockURL + "/api/issues");
-    //
-    //     request.setHeader("Accept", "application/json");
-    //     request.setHeader("Content-type", "application/json");
-    //
-    //     String json = "{ \"title\": null}";
-    //     StringEntity params = new StringEntity(json);
-    //     request.setEntity(params);
-    //
-    //     HttpResponse httpResponse = httpClient.execute(request);
-    //
-    //     int responseStatusCode = httpResponse.getStatusLine().getStatusCode();
-    //     System.out.println("Response Status Code: " + responseStatusCode);
-    //
-    //     assertEquals(415, responseStatusCode);
-    // }
-    //
-    // @Test
-    // public void getIssueHttpOkay() throws IOException {
-    //
-    //     System.out.println("getIssue A-Okay");
-    //
-    //     wireMockServer.stubFor(get(urlMatching("/api/issues/58373"))
-    //             .willReturn(aResponse()
-    //                     .withStatus(HttpStatus.OK.value())
-    //             )
-    //     );
-    //
-    //     HttpGet request = new HttpGet(wireMockURL + "/api/issues/58373");
-    //
-    //     HttpResponse httpResponse = httpClient.execute(request);
-    //
-    //     int responseStatusCode = httpResponse.getStatusLine().getStatusCode();
-    //     System.out.println("Response Status Code: " + responseStatusCode);
-    //
-    //     assertEquals(200, responseStatusCode);
-    // }
-    //
-    // @Test
-    // public void getIssueHttpNotOkay() throws IOException {
-    //     System.out.println("getIssue A-Not-Okay");
-    //
-    //     wireMockServer.stubFor(get(urlMatching("/api/issues/58373"))
-    //             .willReturn(aResponse()
-    //                     .withStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
-    //             )
-    //     );
-    //
-    //     HttpGet request = new HttpGet(wireMockURL + "/api/issues/58373");
-    //
-    //     HttpResponse httpResponse = httpClient.execute(request);
-    //
-    //     int responseStatusCode = httpResponse.getStatusLine().getStatusCode();
-    //     System.out.println("Response Status Code: " + responseStatusCode);
-    //
-    //     assertEquals(415, responseStatusCode);
-    // }
+    @Test
+    public void getIssuesWithSearchTerm() {
+
+        wireMockServer.stubFor(get(urlEqualTo("/Issues"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withBodyFile("issue_test.json")
+                )
+        );
+
+        SearchIssues searchIssues = new SearchIssues();
+        searchIssues.setTitle("marvel universe");
+        ResponseEntity<List<IssueDTO>> issuesResponse = issueController.getIssues(searchIssues);
+
+        assertNotNull(issuesResponse);
+        List<IssueDTO> issues = issuesResponse.getBody();
+        assertNotNull(issues);
+        assertEquals(1, issues.size());
+        IssueDTO issue = issues.get(0);
+        assertNotNull(issue);
+        assertEquals(BigDecimal.valueOf(54781), issue.getId());
+        assertEquals("All-New, All-Different Marvel Universe (2015) #1", issue.getTitle());
+    }
 }
